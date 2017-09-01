19de0b81cac9253338c8e55c1b08d8e5fdbf7de0
Carlos Anguiano <c.joseanguiano@gmail.com>
2017-09-01 13:08:03 -0500

    Init 01

---
+++ app/src/main/java/com/joseanguiano/c/galeria01/activities/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.joseanguiano.c.galeria01.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.joseanguiano.c.galeria01.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
